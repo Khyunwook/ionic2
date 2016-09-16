33adc1b9af4977279c4f4a92ff8c76731fb4bcf9
Khyunwook <khwbori@gmail.com>
2016-09-16 15:16:26 +0900

    Update import.ts

--- import.ts
+++ import.ts
@@ -1,2 +1,5 @@
+import { Http } from '@angular/http'; 
+//  allow us to send requests to the Reddit or Other API
+
 import { FORM_DIRECTIVES } from '@angular/common'; 
 //FORM_DIRECTIVES imports all the form related directives from the angular library
